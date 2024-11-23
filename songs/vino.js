fd092ddf9771a7b759aebf626661f14e17a2bb73
Alberto Fuentes <21albertoff@gmail.com>
2024-11-23 19:10:29 +0100

    New content

--- songs/vino.js
+++ songs/vino.js
@@ -1,18 +1,28 @@
 // Lista de canciones
 const songs = [
-    {
-        imagen: "covers/cover1.jpeg",
-        tituloCancion: "Canción 1",
-        audioCancion: "mp3/audio1.mp3"
-    },
-    {
-        imagen: "covers/cover2.jpeg",
-        tituloCancion: "Canción 2",
-        audioCancion: "mp3/audio2.mp3"
-    },
-    {
-        imagen: "covers/cover3.jpg",
-        tituloCancion: "Canción 3",
-        audioCancion: "mp3/audio3.mp3"
-    }
+  {
+    imagen: "covers/Mónaco - Sped Up.jpg",
+    tituloCancion: "Mónaco - Sped Up",
+    audioCancion: "mp3/Mónaco - Sped Up.weba",
+  },
+  {
+    imagen: "covers/Lo que fuimos.jpg",
+    tituloCancion: "Lo que fuimos",
+    audioCancion: "mp3/Lo que fuimos.weba",
+  },
+  {
+    imagen: "covers/cover1.jpeg",
+    tituloCancion: "Canción 1",
+    audioCancion: "mp3/audio1.mp3",
+  },
+  {
+    imagen: "covers/cover2.jpeg",
+    tituloCancion: "Canción 2",
+    audioCancion: "mp3/audio2.mp3",
+  },
+  {
+    imagen: "covers/cover3.jpg",
+    tituloCancion: "Canción 3",
+    audioCancion: "mp3/audio3.mp3",
+  },
 ];
